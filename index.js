6ee717823a6ef75bc38fd4cc271d65b1d717dc68
prashant226 <prashantnaraniya2003@gmail.com>
2023-11-07 20:25:12 +0530

    commited

---
+++ index.js
@@ -0,0 +1,17 @@
+var name = "prashant";
+var age = 20;
+var users = ["harkirat", "smarth"];
+
+var user1 = 'harkirat';
+var age1 = 20;
+
+
+
+
+
+//OBJECTS
+
+var user1 = {
+    name1: "harkirat",
+    age11: 21
+}
