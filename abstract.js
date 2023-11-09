8bc43216fae60ffb1db3968266bb23bef9bb2003
prashant226 <prashantnaraniya2003@gmail.com>
2023-11-09 22:59:37 +0530

    commited

--- abstract.js
+++ abstract.js
@@ -3,3 +3,7 @@ console.log(+0 == -0);
 console.log("sanket" === 'sanket')
 console.log(null === null)
 console.log({} == {})
+console.log(undefined == null);
+console.log(null == undefined);
+console.log(10 == '10')
+console.log(10 == '12');
