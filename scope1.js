4ef3d0a3afdaafef5dbaf591f8305cb7edae3fdd
prashant226 <prashantnaraniya2003@gmail.com>
2023-11-10 23:20:32 +0530

    commited

---
+++ scope1.js
@@ -0,0 +1,22 @@
+ // GLOBAL
+ var name = 'prashant';
+
+ function fun() {
+     var name1 = "prashnat";
+     console.log("greeting", name1);
+
+     //  function test() {
+     //      console.log("test");
+     //  }
+ }
+
+
+
+ function greet() {
+     console.log("hav fun", name)
+
+ }
+
+ greet();
+ fun();
+ //  test();
