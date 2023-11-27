0b49787b6321aa5aff97b0648bad270f30303d64
prashant226 <prashantnaraniya2003@gmail.com>
2023-11-27 20:55:12 +0530

    commited

---
+++ asynch.js
@@ -0,0 +1,12 @@
+function counter(start, end, delay) {
+    console.log(start);
+
+    // if (start < end) {
+    //     setTimeout(function() {
+    //         counter(start + 1, end, delay);
+    //     }, delay);
+    // }
+}
+
+// Example usage: Count from 1 to 5 with a delay of 1000 milliseconds (1 second)
+counter(1, 5, 1000);
