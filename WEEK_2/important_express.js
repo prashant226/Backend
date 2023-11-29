ca7825856b25f9002d0d8b5667ef7728585c7e1b
prashant226 <prashantnaraniya2003@gmail.com>
2023-11-29 21:31:50 +0530

    commited

--- WEEK_2/important_express.js
+++ WEEK_2/important_express.js
@@ -9,6 +9,10 @@
 // var express = require("express");
 
 // var app = express();
+const express = require('express')
+const app = express()
+const port = 3000;
+
 
 function calculateSum(counter) {
     var sum = 0;
@@ -18,38 +22,41 @@ function calculateSum(counter) {
     return sum;
 }
 
+app.use(middleWare); //a way to register a middleware
+
+function middleWare(req, res, next) {
+    console.log("from inside middleware " + req.headers.counter);
+    res.send("error is present");
+}
+
 
 
 /**express code in beginning  */
-const express = require('express')
-const app = express()
-const port = 3000;
 
 
 /**req-->request
  * res-->response
  */
 
-/**
-    function handleFirstRequest(req, res) 
-{
-    var counter = req.query.counter;
+function handleFirstRequest(req, res) {
+    // var counter = req.query.counter;
+    console.log(req.headers);
+    var counter = req.headers.counter;
     var calculatedSum = calculateSum(counter);
     // console.log(calculatedSum);
     var answer = "the sum is   " + calculatedSum;
     res.send(answer);
 }
-*/
 
 
 function CreateUser(req, res) {
     res.send("hello world!");
 }
-app.get('/handleSum', handleFirstRequest)
-    /**app.get('/:username', handleFirstRequest)
-     * here user_name acts a wildcard which means you can send anything in the route
-     */
-app.post('/createUser', CreateUser)
+// app.get('/handleSum', handleFirstRequest)
+/**app.get('/:username', handleFirstRequest)
+ * here user_name acts a wildcard which means you can send anything in the route
+ */
+app.post('/handleSum', handleFirstRequest)
     /**handleSum is the ROUTE */
 
 
---
+++ WEEK_2/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+// const fs = require("fs");
+// /** fs is the external libray which reads from file*/
+// const express = require("express");
+
+// function callbackfn(err, data) {
+//     console.log(data);
