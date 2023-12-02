c2f49cf55611f63de194dfd48b97261ceecf0667
prashant226 <prashantnaraniya2003@gmail.com>
2023-12-03 00:12:41 +0530

    commit

--- WEEK_2/important_express.js
+++ WEEK_2/important_express.js
@@ -16,14 +16,23 @@ var bodyParser = require('body-parser')
 let numberOfRequests = 0;
 
 
+
+
 function calculateSum(counter) {
     var sum = 0;
-    for (var i = 0; i <= counter; i++) {
+    for (var i = 1; i <= counter; i++) {
         sum = sum + i;
     }
     return sum;
 }
-
+//3==> 1*2*3==6
+function calculateMul(counter) {
+    var sum = 1;
+    for (var i = 1; i <= counter; i++) {
+        answer = answer * i;
+    }
+    return answer;
+}
 //a way to register a middleware
 
 // function middleWare(req, res, next) {
@@ -44,18 +53,38 @@ function calculateSum(counter) {
 
 function handleFirstRequest(req, res) {
     // var counter = req.query.counter;
-    console.log(req.body);
+    // console.log(req.body);
     /**
      * var counter= req.headers.counter this means we are sending counter value in the postman  
      */
-    var counter = req.headers.counter;
-    var calculatedSum = calculateSum(counter);
+
+    var counter = req.body.counter;
     // console.log(calculatedSum);
-    var answer = "the sum is   " + calculatedSum;
-    res.send(answer);
+    // if (counter < 100000) {
+    var calculatedSum = calculateSum(counter);
+    var calculatedMul = calculateMul(counter);
+
+    /**var stringAnswer = "sum is " + calculateSum + "and mul is " + calculatedMul;
+     * accessing like this actually becomes very difficult so we use Of Objects */
+
+    /**if we are putting an object then we are returning in JSON */
+    var answerObject = {
+        sum: calculatedSum,
+        mul: calculatedMul,
+    };
+
+    //     var answer = "the sum is   " + calculatedSum;
+    //     res.status(200).send(answerObject);
+    //     res.status(401).send(answer);
+    // } else {
+
+
+    res.status(200).send(answerObject);
 }
 
-app.use(bodyParser.json()) /**the above calls body middleware */
+
+app.use(bodyParser.json())
+    /**the above calls body middleware 
 
 function CreateUser(req, res) {
     res.send("hello world!");
@@ -64,7 +93,16 @@ function CreateUser(req, res) {
 /**app.get('/:username', handleFirstRequest)
  * here user_name acts a wildcard which means you can send anything in the route
  */
+
+
+function givePage(req, res) {
+    res.send(` <body>
+    <title>Hello from page</title>
+    <b>hi there</b>
+  </body>`)
+}
 app.post('/handleSum', handleFirstRequest)
+app.get('/', givePage)
     /**handleSum is the ROUTE */
 
 
