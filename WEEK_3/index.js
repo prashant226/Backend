943666d60f9ce2e6d6ce3c5f1db28bdb09f66860
prashant226 <prashantnaraniya2003@gmail.com>
2023-12-03 20:54:11 +0530

    commited

--- WEEK_3/index.js
+++ WEEK_3/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-function callback(err, data) {
-    console.log(data);
-}
 
-fs.readFile("a.txt", "utf-8", callback)
+
+fs.readFile("a.txt", "utf-8", (err, data) => {
+    console.log(data);
+});
